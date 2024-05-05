c874a3a837853dffd8bf942d7328a18d3b4a7b35
RefiqQanizade2012 <134130684+RefiqQanizade2012@users.noreply.github.com>
2024-05-05 15:51:31 +0400

    Update script.js

--- script.js
+++ script.js
@@ -41,3 +41,58 @@ registrationForm.addEventListener("submit", function(event) {
         alert("Пожалуйста, заполните все поля.");
     }
 });
+function move() {
+    var elem = document.getElementById("myBar");
+    var width = 0;
+    var id = setInterval(frame, 10);
+    function frame() {
+        if (width >= 100) {
+            clearInterval(id);
+        } else {
+            width++;
+            elem.style.width = width + "%";
+        }
+    }
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+    // Запуск анимации Progress Bar
+    move();
+});
+// Получаем высоту страницы
+var maxHeight = document.body.scrollHeight - window.innerHeight;
+// Получаем высоту видимой области окна браузера
+var winHeight = window.innerHeight;
+// Получаем прогресс-бар
+var progressBar = document.getElementById("myBar");
+window.onscroll = function() {
+    // Получаем текущую прокрутку страницы и высоту видимой области окна
+    var scrolled = window.pageYOffset;
+    
+    // Рассчитываем прогресс прокрутки
+    var progress = (scrolled / maxHeight) * 100;
+    
+    // Устанавливаем ширину прогресс-бара
+    progressBar.style.width = progress + "%";
+};
+$(document).ready(function () {
+    var slider = $('.slider').bxSlider({
+        pagerCustom: '.slider-nav',
+        infiniteLoop: false,
+        hideControlOnEnd: true,
+    });
+
+    $('.slider img').click(function() {
+        var index = $(this).parent().index();
+        slider.goToSlide(index);
+        $('.slider-nav a').removeClass('active');
+        $('.slider-nav a[data-slide-index="' + index + '"]').addClass('active');
+    });
+
+    $('.slider-nav a').click(function() {
+        var index = $(this).data('slide-index');
+        slider.goToSlide(index);
+        $('.slider-nav a').removeClass('active');
+        $(this).addClass('active');
+    });
+});
